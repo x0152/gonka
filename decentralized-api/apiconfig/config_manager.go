c477342ad058fcd256bcf8c09160ac90c91eedde
e.evdomashenko <e.evdomashenko@sapiens.solutions>
2026-05-28 22:37:23 +0300

    feat: enable query cache by default

--- decentralized-api/apiconfig/config_manager.go
+++ decentralized-api/apiconfig/config_manager.go
@@ -520,6 +520,11 @@ func readConfig(provider koanf.Provider) (Config, error) {
 	if err != nil {
 		log.Fatalf("error loading env: %v", err)
 	}
+	if !k.Exists("chain_node.query_cache_enabled") {
+		if err := k.Set("chain_node.query_cache_enabled", true); err != nil {
+			log.Fatalf("error setting default chain_node.query_cache_enabled: %v", err)
+		}
+	}
 	var config Config
 	err = k.Unmarshal("", &config)
 	if err != nil {
